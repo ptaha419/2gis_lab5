dd826e562b2d6da85f2f751167813e61f1a17602
ptaha <ptaha@email.ru>
2026-05-20 19:57:55 +0300

    Внесены изменения в Mapgl.tsx

--- src/Mapgl.tsx
+++ src/Mapgl.tsx
@@ -1,90 +1,127 @@
 import { useEffect } from 'react';
 import { load } from '@2gis/mapgl';
 import { useMapglContext } from './MapglContext';
-import { Clusterer } from '@2gis/mapgl-clusterer';
-import { RulerControl } from '@2gis/mapgl-ruler';
-import { Directions } from '@2gis/mapgl-directions';
 import { useControlRotateClockwise } from './useControlRotateClockwise';
 import { ControlRotateCounterclockwise } from './ControlRotateConterclockwise';
 import { MapWrapper } from './MapWrapper';
 
-export const MAP_CENTER = [55.31878, 25.23584];
+import { FeatureCollection, Geometry, GeoJsonProperties } from 'geojson';
+import geoData from './data/small_data.json';
+
+export const MAP_CENTER = [82.913021, 54.992394];
 
 export default function Mapgl() {
-    const { setMapglContext } = useMapglContext();
+  const { setMapglContext } = useMapglContext();
 
-    useEffect(() => {
-        let map: mapgl.Map | undefined = undefined;
-        let directions: Directions | undefined = undefined;
-        let clusterer: Clusterer | undefined = undefined;
+  useEffect(() => {
+    let map: mapgl.Map | undefined = undefined;
 
-        load().then((mapgl) => {
-            map = new mapgl.Map('map-container', {
-                center: MAP_CENTER,
-                zoom: 13,
-                key: 'a1893935-6834-4445-b97a-3405fb426c5b',
-            });
+    load().then((mapgl) => {
+      map = new mapgl.Map('map-container', {
+        center: MAP_CENTER,
+        zoom: 13,
+        key: 'da014549-c5b5-4930-a69d-1e67b2ec7626',
+        style: 'a7f8b38b-10c9-4ab0-84c3-614c92c3df53',
+      });
 
-            map.on('click', (e) => console.log(e));
+      const data: FeatureCollection<Geometry, GeoJsonProperties> =
+        geoData as FeatureCollection<Geometry, GeoJsonProperties>;
 
-            /**
-             * Ruler  plugin
-             */
+      const source = new mapgl.GeoJsonSource(map, {
+        data,
+        attributes: {
+          visible: true, // Уникальное свойство
+        },
+      });
 
-            const rulerControl = new RulerControl(map, { position: 'centerRight' });
+      // const layer1 = {
+      //   id: 'dtp-data-layer',
+      //   // Тип объекта отрисовки
+      //   type: 'point',
+      //   // Стиль объекта отрисовки
+      //   style: {
+      //     iconImage: ['match', ['get', 'color'],
+      //     ['blue'], 'ent_i', 'ent'],
+      //     iconWidth: 15,
+      //     textField: ['get', 'category'],
+      //     textFont: ['Noto_Sans'],
+      //     textColor: '#0098ea',
+      //     textHaloColor: '#fff',
+      //     textHaloWidth: 1,
+      //     iconPriority: 100,
+      //     textPriority: 100,
+      //   },
+      // };
 
-            /**
-             * Clusterer plugin
-             */
+      // map.on('styleload', () => {
+      //   map?.addLayer(layer1);
+      // });
 
-            clusterer = new Clusterer(map, {
-                radius: 60,
-            });
+      // const layer2: any = {
+      //   id: 'dtp-heatmap-layer', 
+      //   filter: [
+      //     'match',
+      //     ['sourceAttr', 'visible'],
+      //     [true],
+      //     true, // Значение при совпадении атрибута 'purpose' источника со значением 'heatmap'
+      //     false, // Значение при несовпадении
+      //   ],
+      //   // Тип объекта отрисовки
+      //   type: 'heatmap',
+      //   // Стиль объекта отрисовки
+      //   style: {
+      //   color: [
+      //   'interpolate',
+      //   ['linear'],
+      //   ['heatmap-density'],
+      //   0,
+      //   'rgba(0, 0, 0, 0)',
+      //   0.2,
+      //   '#61589A',
+      //   0.4,
+      //   '#9A83C9',
+      //   0.6,
+      //   '#CCB0E4',
+      //   0.8,
+      //   '#F0E1F5',
+      //   1,
+      //   'rgba(255, 255, 255, 1)',
+      //   ],
+      //   radius: 20,
+      //   intensity: 0.8,
+      //   opacity: 0.8,
+      //   downscale: 1,
+      //   },
+      // };
 
-            const markers = [
-                { coordinates: [55.27887, 25.21001] },
-                { coordinates: [55.30771, 25.20314] },
-                { coordinates: [55.35266, 25.24382] },
-            ];
-            clusterer.load(markers);
+      // map.on('styleload', () => {
+      // map?.addLayer(layer2);
+      // });
 
-            /**
-             * Directions plugin
-             */
+      setMapglContext({
+        mapglInstance: map,
+        mapgl,
+      });
+    });
 
-            directions = new Directions(map, {
-                directionsApiKey: 'rujany4131', // It's just demo key
-            });
+    // Destroy the map if the Map component is going to be unmounted
+    return () => {
+      if (map) {
+        map.destroy();
+      }
+      setMapglContext({
+        mapglInstance: undefined,
+        mapgl: undefined,
+      });
+    };
+  }, [setMapglContext]);
 
-            directions.carRoute({
-                points: [
-                    [55.28273111108218, 25.234131928828333],
-                    [55.35242563034581, 25.23925607042088],
-                ],
-            });
+  useControlRotateClockwise();
 
-            setMapglContext({
-                mapglInstance: map,
-                rulerControl,
-                mapgl,
-            });
-        });
-
-        // Destroy the map, if Map component is going to be unmounted
-        return () => {
-            directions && directions.clear();
-            clusterer && clusterer.destroy();
-            map && map.destroy();
-            setMapglContext({ mapglInstance: undefined, mapgl: undefined });
-        };
-    }, [setMapglContext]);
-
-    useControlRotateClockwise();
-
-    return (
-        <>
-            <MapWrapper />
-            <ControlRotateCounterclockwise />
-        </>
-    );
+  return (
+    <>
+      <MapWrapper />
+      <ControlRotateCounterclockwise />
+    </>
+  );
 }
